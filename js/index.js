8bb4b05a2666922ecf3bc0e84a951367216387cd
AxelMrak <axelmrak@gmail.com>
2022-07-21 19:19:08 -0300

    Changes in JS, I change the way what the results will be show in the div

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
 // Start extract DOM elements
-const btn = document.getElementById("btn");
-const label = document.getElementById("label-input");
-const input = document.getElementById("field");
-const labelIva = document.getElementById("label-input-iva");
-const inputIva = document.getElementById("field-iva");
-// End extract DOM elements
+const btn = document.getElementById(`btn`);
+const label = document.getElementById(`label-input`);
+const input = document.getElementById(`field`);
+const labelVat = document.getElementById(`label-input-vat`);
+const inputVat = document.getElementById(`field-vat`);
+
+
 // Function for calculate IVA
-function calculateIva() {
-  const value1 = document.getElementById("field").value;
-  const value2 = document.getElementById("field-iva").value;
-  const paragraph = document.getElementById("result");
+function calculateVat(event) {
+  const value1 = document.getElementById(`field`).value;
+  const value2 = document.getElementById(`field-vat`).value;
+  const divResults = document.getElementById(`results`);
+  const resultVta = document.getElementById(`result-vta`);
+  const onlyVta = document.getElementById(`only-vta`);
+  const onlyPrice = document.getElementById(`only-price`);
   //Transform object to Number 
   const num = Number(value1);
-  const iva = Number(value2);
+  const vat = Number(value2);
   //IVA CALCULATE
-  let priceWithIva = num * iva / 100;
-  const result = priceWithIva + num;
+  let priceWithVat = num * vat / 100;
+  const result = priceWithVat + num;
   //ADD RESULT TO DOM
-  paragraph.innerHTML = result;
+  resultVta.innerHTML = result;
+  onlyVta.innerHTML = priceWithVat;
+  onlyPrice.innerHTML = num;
 }
+
 // EVENT IN DOM ELEMENT "BUTTON"
-btn.addEventListener("click", calculateIva)
+btn.addEventListener(`click`, calculateVat);
 
 
 
